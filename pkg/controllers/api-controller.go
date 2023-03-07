b0d913c51dd7df26edd08936b8ea9eea8a910621
rjkoh <koh.ruijie123@gmail.com>
2023-03-07 17:21:13 +0800

    implement controllers

--- pkg/controllers/api-controller.go
+++ pkg/controllers/api-controller.go
@@ -1,17 +1,138 @@
 package controllers
 
-func RegisterStudents() {
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"regexp"
 
+	"github.com/rjkoh/golang-assessment-api/pkg/models"
+	"github.com/rjkoh/golang-assessment-api/pkg/utils"
+)
+
+func RegisterStudents(writer http.ResponseWriter, req *http.Request) {
+	RegStudent := &models.Student{IsSuspended: false}
+	utils.ParseBody(req, RegStudent)
+	err := models.AddStudent(*RegStudent)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		er := json.NewEncoder(writer).Encode(struct{ message string }{message: err.Error()})
+		if er != nil {
+			log.Printf("failed to write response: %v", er)
+		}
+		return
+	}
+	writer.WriteHeader(http.StatusNoContent)
+	writer.Header().Set("Content-Type", "application/json")
 }
 
-func FindCommonStudents() {
+func FindCommonStudents(writer http.ResponseWriter, req *http.Request) {
+	teachers, ok := req.URL.Query()["teacher"]
+	if !ok {
+		writer.WriteHeader(http.StatusBadRequest)
+		er := json.NewEncoder(writer).Encode(struct{ message string }{message: "Missing teacher parameter"})
+		if er != nil {
+			log.Printf("failed to write response: %v", er)
+		}
+		return
+	}
 
+	rows := models.FindCommon(teachers)
+
+	type Response struct {
+		Students []string `json:"students"`
+	}
+
+	response := Response{Students: make([]string, 0)}
+	for rows.Next() {
+		var email string
+		err := rows.Scan(&email)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			er := json.NewEncoder(writer).Encode(struct{ message string }{message: err.Error()})
+			if er != nil {
+				log.Printf("failed to write response: %v", er)
+			}
+			return
+		}
+		response.Students = append(response.Students, email)
+	}
+
+	jsonData, err := json.Marshal(response)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		er := json.NewEncoder(writer).Encode(struct{ message string }{message: err.Error()})
+		if er != nil {
+			log.Printf("failed to write response: %v", er)
+		}
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(jsonData)
 }
 
-func SuspendStudent() {
-
+func SuspendStudent(writer http.ResponseWriter, req *http.Request) {
+	email := ""
+	utils.ParseBody(req, email)
+	err := models.Suspend(email)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		er := json.NewEncoder(writer).Encode(struct{ message string }{message: err.Error()})
+		if er != nil {
+			log.Printf("failed to write response: %v", er)
+		}
+		return
+	}
+	writer.WriteHeader(http.StatusNoContent)
+	writer.Header().Set("Content-Type", "application/json")
 }
 
-func RetrieveStudentsForNotification() {
+func RetrieveStudentsForNotification(writer http.ResponseWriter, req *http.Request) {
+	type Noti struct {
+		Teacher      string `json:"teacher"`
+		Notification string `json:"notification"`
+	}
 
+	rcvNoti := Noti{}
+	utils.ParseBody(req, rcvNoti)
+	rows := models.GetNotifiableStudents(getEmails(rcvNoti.Notification), rcvNoti.Teacher)
+
+	type Response struct {
+		Recipients []string `json:"recipients"`
+	}
+
+	response := Response{Recipients: make([]string, 0)}
+	for rows.Next() {
+		var email string
+		err := rows.Scan(&email)
+		if err != nil {
+			writer.WriteHeader(http.StatusBadRequest)
+			er := json.NewEncoder(writer).Encode(struct{ message string }{message: err.Error()})
+			if er != nil {
+				log.Printf("failed to write response: %v", er)
+			}
+			return
+		}
+		response.Recipients = append(response.Recipients, email)
+	}
+
+	jsonData, err := json.Marshal(response)
+	if err != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		er := json.NewEncoder(writer).Encode(struct{ message string }{message: err.Error()})
+		if er != nil {
+			log.Printf("failed to write response: %v", er)
+		}
+		return
+	}
+
+	writer.WriteHeader(http.StatusOK)
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Write(jsonData)
+}
+
+func getEmails(str string) []string {
+	re := regexp.MustCompile(`\b\w+@\w+\.\w+\b`)
+	return re.FindAllString(str, -1)
 }
--- pkg/models/students.go
+++ pkg/models/students.go
@@ -1,6 +1,115 @@
 package models
 
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+)
+
 type Student struct {
-	email   string `form:"email" json:"email"`
-	teacher string `form:"teacher" json:"teacher"`
+	Email       string `form:"email" json:"email"`
+	Teacher     string `form:"teacher" json:"teacher"`
+	IsSuspended bool   `form:"isSuspended" json:"isSuspended"`
+}
+
+func AddStudent(stud Student) error {
+	query := "INSERT INTO Student(email, teacher, isSuspended) VALUES (?, ?, ?)"
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancelfunc()
+
+	stmt, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when preparing", err)
+		return err
+	}
+
+	res, err := stmt.ExecContext(ctx, stud.Email, stud.Teacher, stud.IsSuspended)
+	if err != nil {
+		log.Printf("Error %s when executing", err)
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when finding students affected", err)
+		return err
+	}
+
+	log.Printf("%d student created, %+v", rows, stud)
+
+	return nil
+}
+
+func FindCommon(teachers []string) *sql.Rows {
+	query := "SELECT email FROM Student WHERE teacher = " + teachers[0]
+
+	for i := 1; i < len(teachers); i++ {
+		intersect := "INTERSECT SELECT email FROM Student WHERE teacher = " + teachers[i]
+		query = query + " " + intersect
+	}
+
+	rows, err := db.Query(query)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	return rows
+}
+
+func Suspend(email string) error {
+	query := "UPDATE Student SET isSuspended = TRUE WHERE email = ?"
+
+	ctx, cancelfunc := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancelfunc()
+
+	stmt, err := db.PrepareContext(ctx, query)
+	if err != nil {
+		log.Printf("Error %s when preparing", err)
+		return err
+	}
+
+	res, err := stmt.ExecContext(ctx, email)
+	if err != nil {
+		log.Printf("Error %s when executing", err)
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		log.Printf("Error %s when finding students affected", err)
+		return err
+	}
+
+	log.Printf("%d student suspended, %s", rows, email)
+
+	return nil
+}
+
+func GetNotifiableStudents(emails []string, teacher string) *sql.Rows {
+	withTeacherQuery := fmt.Sprintf("SELECT email FROM Student WHERE teacher = %s AND isSuspended = FALSE", teacher)
+
+	mentionedQuery := "SELECT email FROM Student WHERE email = " + emails[0]
+
+	for i := 1; i < len(emails); i++ {
+		next := "OR email = " + emails[i]
+		mentionedQuery += next
+	}
+
+	query := withTeacherQuery + " UNION " + mentionedQuery
+
+	rows, err := db.Query(query)
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	return rows
 }
